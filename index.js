19e03eacde00939cf32f3c32bcf149363e45a3f6
RenMai <mtnhan@apcs.vn>
2019-11-07 13:31:56 +0200

    Hi

---
+++ index.js
@@ -0,0 +1,18 @@
+  GNU nano 2.3.1                            File: index.js                                                              
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+        res.send('Hello from my Node server');
+});
+app.get('demo', (req, res) => {
+        res.send('demo');
+});
+
+app.listen(3000, () => {
+        console.log('Server app start?');
+});
+
